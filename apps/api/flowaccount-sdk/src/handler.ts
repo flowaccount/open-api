e585a80090d82efa3778c3c1d832f943dc46f783
Kriangsak@Tak <tak@flowaccount.com>
2021-12-10 12:33:44 +0700

    add example create cash invoice

--- apps/api/flowaccount-sdk/src/handler.ts
+++ apps/api/flowaccount-sdk/src/handler.ts
@@ -1,15 +1,20 @@
+import 'reflect-metadata'
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import { container } from 'tsyringe'
+import { CashInvoiceService } from './cashInvoiceService'
 
-export const helloWorld: APIGatewayProxyHandler = async (event, _context, callback) => {
+export const createCashInvoice: APIGatewayProxyHandler = async (event, _context, callback) => {
+
+  // add dependency
+  container.register<CashInvoiceService>('cashInvoiceService', { useClass: CashInvoiceService })
+
+  // create document
+  const cashInvoiceService = container.resolve<CashInvoiceService>('cashInvoiceService')
+  const result = await cashInvoiceService.create()
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: result ? 'true' : 'false',
   }
 }
+
